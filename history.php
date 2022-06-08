d7e30fead1c84bcdbe364ba846af9e4a4c218a06
stefiucmarina <79245738+stefiucmarina@users.noreply.github.com>
2022-06-08 20:25:15 +0300

    Display matches from a chosen day

---
+++ history.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html Lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="history_style.css">
+    <title>History</title>
+
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <h4>HISTORY</h4>
+        </div>
+        <ul class="nav-links">
+            <li><a href="../About/about.html">About</a></li>
+            <li><a href="../Profile/profile.html">Profile</a></li>
+            <li><a href="../Races/races.html">Races</a></li>
+            <li><a href="../Report/report.html">Report</a></li>
+
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+
+        </div>
+    </nav>
+
+    <div class="content">
+
+        <form class="form_date" method="post" autocomplete="off">
+            <label for="match_day">Choose a date:</label>
+            <input type="date" id="match_day" name="match_day">
+            <input class="bsubmit" type="submit" name="Search">
+            <?php
+                $conn = mysqli_connect("localhost", "root", "", "tw");
+         
+                // Check connection
+                if($conn === false){
+                    die("ERROR: Could not connect. "
+                    . mysqli_connect_error());
+                }
+
+                if(isset($_POST["Search"]))
+                {
+                    $selecttxt=$_POST["match_day"];
+                    $query=mysqli_query($conn,"select * from races where Date='$selecttxt'");
+                    $count=mysqli_num_rows($query);
+                    if ($count=="0")
+                    {
+                        echo "<h2> No records found! </h2>";
+                    }
+                    else
+                    {
+                      ?>
+                      <table border="1">
+                      <tr>
+                        <th>ID</th>
+                        <th>Race</th>
+                        <th>Date</th>
+                        <th>Winner</th>
+                        <th>Outcome</th>
+                      </tr>
+                      <?php
+                      while ($row = mysqli_fetch_array($query))
+                      {
+                          echo "<tr><td>{$row["ID"]}</td><td>{$row["Race"]}</td><td>
+                                {$row["Date"]}</td><td>{$row["Winner"]}</td>
+                                <td>{$row["Outcome"]}</td></tr>\n";
+                                
+                      }
+                      
+                    }
+                }
+            ?>
+        </form>
+
+
+
+        <!-- <div class="races">
+            <table id="matches">
+                <tr>
+                    <th>Race</th>
+                    <th>Date</th>
+                    <th>Winner</th>
+                    <th>Outcome</th>
+                </tr>
+                <tr>
+                    <td>Pablo vs. Escobar</td>
+                    <td>10.04.2022</td>
+                    <td>Pablo</td>
+                    <td>+20$</td>
+                </tr>
+                <tr>
+                    <td>Yuki vs. Cookie</td>
+                    <td>10.04.2022</td>
+                    <td>Cookie</td>
+                    <td>-</td>
+                </tr>
+            </table>
+        </div> -->
+
+        
+    <button onclick="location.href='../Races/races.html'" class="braces">Try your luck now!</button>
+    </div>
+
+
+
+    <script src="app.js"></script>
+</body>
+
+
+
+
+
+</html>
