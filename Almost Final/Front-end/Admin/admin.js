962ebafddf742f3eac18037176027a3cb72445a2
Begu Maria Florina <maria.begu.florina@gmail.com>
2022-06-13 07:31:50 +0300

    Update

--- Almost Final/Front-end/Admin/admin.js
+++
@@ -1,8 +0,0 @@
-function popup(id) {
-    var x = document.getElementById(id);
-    if (x.classList.contains("active")) {
-      x.classList.remove("active")
-    } else {
-      x.classList.add("active")
-    }
-  }
